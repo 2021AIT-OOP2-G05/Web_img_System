e00437a4b2343ccb6db0274ba512d5ca3d1a8a7d
k20022 <gandam.astalot.blue@gmail.com>
2021-12-16 21:50:34 +0900

    Update web.py

--- web.py
+++ web.py
@@ -35,18 +35,55 @@ def uploads_get():
 
     return render_template("upload.html", file_list = fileURLlist)
 
+
+#gray_fileの画像を取得してupload.htmlと一緒に返す
+@app.route("/gray_file_get/")
+def uploads_get():
+    files = glob.glob("./gray_file/*")
+    fileURLlist = []
+    for file in files:
+        fileURLlist.append("/grayscaling/" + os.path.basename(file))
+    print(fileURLlist)
+
+    return render_template("upload.html", file_list = fileURLlist)
+
+
+#Canny_fileの画像を取得してupload.htmlと一緒に返す
+@app.route("/Canny_file_get/")
+def uploads_get():
+    files = glob.glob("./Canny_file/*")
+    fileURLlist = []
+    for file in files:
+        fileURLlist.append("/Canny_filter/" + os.path.basename(file))
+    print(fileURLlist)
+
+    return render_template("upload.html", file_list = fileURLlist)
+
+
+
 #cssを返す
 @app.route('/uploads_get/<path:filename>')
 def send_css(filename):
     return send_from_directory('./templates', filename)
 
-#画像を返す
+
+#uploadsの画像を返す
 @app.route('/uploaded/<path:filename>')
 def uploaded_file(filename):
     return send_from_directory('./uploads', filename)
 
 
-    
+#gray_fileの画像を返す
+@app.route('/grayscaling/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory('./gray_file', filename)
+
+
+#gray_fileの画像を返す
+@app.route('/Canny_filter/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory('./Canny_file', filename)
+
 
 
 if __name__ == "__main__":

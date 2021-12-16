8869496cfed65beba152e2eba639702441c381ca
LiLy-0000 <0et7m62438d382g@ezweb.ne.jp>
2021-12-16 17:11:24 +0900

    変更
    ・#on_created -> on_modifiedに変更
    ・image.pyを追加（林が書いてくれたやつ）
    ・重複している文の削除

--- watchdog.py
+++ watchdog.py
@@ -1,69 +1,36 @@
-
-'''林
-import cv2
-import numpy as np
-import os
-
-画像のグレースケール化（二値化していない）
-
-def gray_scale(image):
-    
-    #im = cv2.imread(color_img)
-    #color_img = cv2.imread(image)
-    #im_gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
-
-    #cv2.imwrite('output1.jpeg', im_gray)
-
-    #フォルダ作成(使うかは未定)
-    #os.makedir.s('gray_directory', exist_ok = True)
-
-Cannyによる画像検出
-
-def canny_filta(image):
-    img = cv2.imread(image)
-    img_canny = cv2.Canny(img, 100, 200)
-    cv2.imwrite('output2.jpeg', img_canny)
-
-    #ファイル作成(使うかは未定)
-    #os.makedirs('canny_directory', exist_ok = True)
-
-
-
-if __name__ == "__main__":
-    gray_scale()
-    canny_filta()
-    '''
-
-
 #wathdogを使用する際はターミナル等で pip install watchdog　を実行しインストールしないと使えないかも?しれません。
 
 import time
 import sys
+import os
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
 
-from image import image #画像処理用のプログラム
+from image import gray_scale, canny_filta #画像処理用のプログラム
 
 #import 実行したい画像処理プログラム
 
 
-target_dir = "監視したいフォルダのパス"
+target_dir = "uploads" #uploadsフォルダを監視フォルダに指定
 
 #LoggingEvenHandlerを上書きして動作を変更
 class LoggingEventHandler2(FileSystemEventHandler):
-    def on_created(self, event):    
+    #ファイルやフォルダが更新された場合
+    def on_modified(self, event):    #on_created -> on_modifiedに変更
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        gray_scale(filename)    #関数を実行
+        canny_filta(filename)   #関数を実行
 
-        print(event.src_path+"が生成されました。")#debug用おk
+        print(filepath+"が生成されました。")#debug用おk
         #ファイルに画像が入ってきた時に画像処理をするプログラムを起動
+        
+
+
 
 if __name__ == "__main__":
-    path = target_dir #監視ファイルのパス
-
-    print("生成されました。" + event.src_path)
-
-if __name__ == "__main__":
-    path = target_dir
+    path = target_dir #監視フォルダのパス
 
     event_handler = LoggingEventHandler2()
     observer = Observer()       #監視オブジェクト生成

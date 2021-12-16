1d49eb91e8b0315162881c91d27cdf2fbd714468
k20022 <gandam.astalot.blue@gmail.com>
2021-12-16 21:10:37 +0900

    web.pyの更新（その他いろいろ）
    upload.htmlの画像用のcssを追加
    web.pyにuploadsの画像をwebページ上で表示する処理を追加

--- web.py
+++ web.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, redirect ,render_template, jsonify
+from flask import Flask, request, redirect ,render_template, jsonify, send_from_directory
 import json  # Python標準のJSONライブラリを読み込んで、データの保存等に使用する
 import os
 import glob
 
 
 app = Flask(__name__)
-app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
+app.config["JSON_AS_ASCII"] = False 
 
-# TOPページ(本当は別のWebサーバーを構築するのが良い)
-# http://127.0.0.1:5000/
+
 @app.route('/')
 def index():
     return render_template("index.html")
-
 
 
 
@@ -24,17 +22,34 @@ def upload():
         file.save(os.path.join(upload_folder ,file.filename)) #file.filenameでファイル名取得
         return redirect("/")
     else: return redirect("/")
-  
-@app.route("/upload_get/")
+
+
+#uploadsの画像を取得してupload.htmlと一緒に返す
+@app.route("/uploads_get/")
 def uploads_get():
-    #
     files = glob.glob("./uploads/*")
-    filename = []
+    fileURL = []
     print(files)
     for file in files:
-        filename.append("/uploads/" + os.path.basename(file))
+        fileURL.append("/uploaded/" + os.path.basename(file))
+    print(fileURL)
 
-    return render_template("upload.html", file_list = filename)
+    return render_template("upload.html", file_list = fileURL)
+
+#cssを返す
+@app.route('/uploaded/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory('./uploads', filename)
+
+
+
+@app.route('/uploaded/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory('./uploads', filename)
+
+
+    
+
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
